504a044e2ec3a8c39866791e88330d25a2470783
Justin Anderson <justin@watchworddigital.com>
2021-11-08 15:27:34 -0800

    Set state without default value

--- examples/08-character-card/src/Application.tsx
+++ examples/08-character-card/src/Application.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import { useState, useEffect } from 'react';
 
 import { CharacterType, fetchCharacter } from './characters';
 
@@ -6,7 +6,17 @@ import { Loading } from './Loading';
 import { CharacterInformation } from './CharacterInformation';
 
 const Application = () => {
-  return <main></main>;
+  const [character, setCharacter] = useState<CharacterType | null>(null);
+
+  useEffect(() => {
+    fetchCharacter().then((c) => {
+      setCharacter(c);
+    });
+  }, []);
+
+  return (
+    <main>{character && <CharacterInformation character={character} />}</main>
+  );
 };
 
 export default Application;
