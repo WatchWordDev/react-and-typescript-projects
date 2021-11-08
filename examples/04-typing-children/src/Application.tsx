3c36f04b88b4168931d806ba38ba13c30d1c788f
Justin Anderson <justin@watchworddigital.com>
2021-11-08 14:52:22 -0800

    WIP fundamentals exercises

--- examples/04-typing-children/src/Application.tsx
+++ examples/04-typing-children/src/Application.tsx
@@ -8,11 +8,14 @@
  * React.ReactChild[];
  */
 
-type BoxProps = { children: React.ReactNode };
+type BoxProps = {
+  children: React.ReactNode;
+  style?: React.CSSProperties;
+};
 
-const Box = ({ children }: BoxProps) => {
+const Box = ({ children, style = {} }: BoxProps) => {
   return (
-    <section style={{ padding: '1em', border: '5px solid purple' }}>
+    <section style={{ padding: '1em', border: '5px solid purple', ...style }}>
       {children}
     </section>
   );
@@ -23,7 +26,7 @@ export default function Application() {
     <Box>
       Just a string.
       <p>Some HTML that is not nested.</p>
-      <Box>
+      <Box style={{ borderColor: 'red' }}>
         <h2>Another React component with one child.</h2>
       </Box>
       <Box>
